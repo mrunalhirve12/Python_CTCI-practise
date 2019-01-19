1d4017b1ffb58328c89793b6f29b032d143aa12c
mrunal <mrunalhirve@gmail.com>
2019-01-19 12:04:33 -0800

    String Urification

---
+++ Arrays and Strings/String_Urification.py
@@ -0,0 +1,21 @@
+# c++ solution - https://www.geeksforgeeks.org/urlify-given-string-replace-spaces/
+# -------To understand step by step solution---------------------------------
+
+
+# Time and space complexity O(n)
+Max = 1000
+
+
+def string_urify(str, str_len):
+    # using for loop
+    str_r = str.rstrip(' ')
+    for i in range(len(str_r)):
+        str1 = str_r.replace(' ', '%20')
+    print(str1)
+
+    # using direct
+    str = str[:str_len].replace(' ', '%20')
+    print(str)
+
+
+string_urify('How are you ', 11)
