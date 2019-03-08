643860a231baca829911af5fe40c177d93737379
mrunal <mrunalhirve@gmail.com>
2019-03-07 22:04:16 -0800

    practise

---
+++ LeetCode/Array/reverse int.py
@@ -0,0 +1,30 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        # res store the result
+        res = 0
+        flag = False
+        if x < 0:
+            flag = True
+            x = -1 * x
+        # till x is greater than 0; reverse the number
+        while x > 0:
+            # example x= 123, 123% 10 gives 3
+            # res initilally 0, res = 3;
+            res = res * 10 + x % 10
+            # x will be less than 1 digit, 123/10 gives 12
+            x = x//10
+        # returns the number
+        if res > pow(2, 31) - 1 or res < -pow(2, 31):
+            return 0
+        elif flag == True:
+            res = -1 * res
+            print(res)
+        else:
+            print(res)
+
+s= Solution
+#Test Case 1
+s.reverse(s,120)
+
+
+
