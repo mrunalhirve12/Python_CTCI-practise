64b0841c5a26045100571d1ea162f3408d345846
mrunal <mrunalhirve@gmail.com>
2019-03-27 08:53:20 -0700

    practise

---
+++ LeetCode/Trees/Level Order Traversal.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+import queue
+from collections import deque
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        #if root is None
+        if root is None:
+            return []
+        #initialize final result list res, queue.
+        res = []
+        queue = []
+        #append root in queue
+        queue.append(root)
+        #while queuenot empty
+        while queue:
+            #take values from queue of each level as list
+            res.append([c.val for c in queue])
+            #initialize new list
+            qq = []
+            #for nodes in queue each level, get it left and right child
+            for node in queue:
+                if node.left is not None:
+                    qq.append(node.left)
+                if node.right is not None:
+                    qq.append(node.right)
+            #append new list in queue
+            queue = qq
+        return res
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+s = Solution()
+s.levelOrder(root)
+
+"""
+Test Case #1
+s.levelOrder([])
+"""
+
