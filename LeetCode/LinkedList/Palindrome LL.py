d3eec0d377be24c5c7875d2f0c0c76b0bbc94581
mrunal <mrunalhirve@gmail.com>
2019-03-30 19:56:35 -0700

    practise

--- LeetCode/LinkedList/Palindrome LL.py
+++ LeetCode/LinkedList/Palindrome LL.py
@@ -13,28 +13,65 @@ Follow up:
 Could you do it in O(n) time and O(1) space?
 """
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
 
 class Solution(object):
-
-    def move(self, root):
-        tmp = root
-        while root.next is not None:
-            tmp = tmp.next
-        return tmp
 
     def isPalindrome(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        if not head:
-            return None
+        #if not head
+        if not head or not head.next:
+            return True
+        slow = head
+        fast = head
 
-        start = head
-        end = self.move(head)
+        #calculate mid
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        #reverse second half of linked list
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+        #compare values
+        while prev and head:
+            if head.val != prev.val:
+                return False
+            head = head.next
+            prev = prev.next
+        return True
+
+
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(2)
+l1.next.next.next = ListNode(1)
+l1.next.next.next.next = ListNode(1)
+
+s = Solution()
+print(s.isPalindrome(l1))
+
+
+"""
+#TESTCASE #1
+[1,2,2,1]
+
+#TESTCASE #1
+[1,2,2,1,1]
+
+#TESTCASE #1
+[]
+"""
 
         
