4b979061d94f56f5cfaf6f28b0e002572256bc82
mrunal <mrunalhirve@gmail.com>
2019-03-31 11:29:43 -0700

    practise

---
+++ LeetCode/Array/Search in 2D Matrix.py
@@ -0,0 +1,62 @@
+"""
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+"""
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        #if no field
+        if not matrix or target is None:
+            return False
+
+        #calculate rows and cols
+        rows, cols = len(matrix), len(matrix[0])
+
+        #calculate lows and highs
+        low, high = 0, rows*cols -1
+
+        while low <= high:
+            mid = int(low + high)/2
+            num = matrix[int(mid/cols)][int(mid%cols)]
+
+            if num == target:
+                return True
+            elif num < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return False
+
+
+mat = [[1,   3,  5,  7],[10, 11, 16, 20],[23, 30, 34, 50]]
+
+s = Solution()
+s.searchMatrix(mat, 3)
