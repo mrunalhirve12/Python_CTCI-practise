d235129f4ad4b18649f4cba4eef5c7cc167ceabe
mrunal <mrunalhirve@gmail.com>
2019-04-04 18:56:11 -0700

    practise

---
+++ LeetCode/Trees/lowest common ancestor binary tree.py
@@ -0,0 +1,86 @@
+"""
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+Example 1:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+Example 2:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+
+
+Note:
+
+All of the nodes' values will be unique.
+p and q are different and both values will exist in the binary tree.
+"""
+# Definition for a binary tree node.
+class TreeNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        # If root null then return None
+        if root == None:
+            return None
+        # If found any of the node then return value to parent
+        if root == p or root == q:
+            return root
+        # go to left and right to find p and q
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        # if both left and right are not null for given node then this is common Ancestor
+        if (left != None and right != None):
+            return root
+        # if both are None then this is not LCA
+        if (left == None and right == None):
+            return None
+        # Either or a
+        if (left != None):
+            return left
+        else:
+            return right
+
+
+
+# Driver program to test above function
+
+# Let us construct the BST shown in the figure
+root = TreeNode(3)
+root.left = TreeNode(5)
+root.right = TreeNode(1)
+root.left.left = TreeNode(6)
+root.left.right = TreeNode(2)
+root.left.right.left = TreeNode(7)
+root.left.right.right = TreeNode(4)
+root.right.left = TreeNode(0)
+root.right.right = TreeNode(8)
+
+
+s = Solution()
+
+n1 = TreeNode(5)
+n2 = TreeNode(1)
+print(s.lowestCommonAncestor(root, n1, n2))
+
+n1 = TreeNode(6)
+n2 = TreeNode(2)
+print(s.lowestCommonAncestor(root, n1, n2))
