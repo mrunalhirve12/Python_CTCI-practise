0e2d0e03a6b4c9182271b8482d82fa1b3164ca1d
mrunal <mrunalhirve@gmail.com>
2019-05-31 11:26:29 -0700

    practise

---
+++ LeetCode/Array/Partition Equal Subset Sum.py
@@ -0,0 +1,19 @@
+"""
+Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+Note:
+
+Each of the array element will not exceed 100.
+The array size will not exceed 200.
+"""
+
+
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+s = Solution()
+s.canPartition()
