5cc086dd20f6a84d5e3beccb78bba58c15fd52b9
mrunal <mrunalhirve@gmail.com>
2019-05-31 20:02:24 -0700

    practise

---
+++ LeetCode/Array/Astroid Collision.py
@@ -0,0 +1,79 @@
+"""
+We are given an array asteroids of integers representing asteroids in a row.
+
+For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+Example 1:
+Input:
+asteroids = [5, 10, -5]
+Output: [5, 10]
+Explanation:
+The 10 and -5 collide resulting in 10.  The 5 and 10 never collide.
+Example 2:
+Input:
+asteroids = [8, -8]
+Output: []
+Explanation:
+The 8 and -8 collide exploding each other.
+Example 3:
+Input:
+asteroids = [10, 2, -5]
+Output: [10]
+Explanation:
+The 2 and -5 collide resulting in -5.  The 10 and -5 collide resulting in 10.
+Example 4:
+Input:
+asteroids = [-2, -1, 1, 2]
+Output: [-2, -1, 1, 2]
+Explanation:
+The -2 and -1 are moving left, while the 1 and 2 are moving right.
+Asteroids moving the same direction never meet, so no asteroids will meet each other.
+Note:
+
+The length of asteroids will be at most 10000.
+Each asteroid will be a non-zero integer in the range [-1000, 1000].
+"""
+
+
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for new in asteroids:
+            if not res:
+                res.append(new)
+            else:
+                if res[-1] < 0:
+                    res.append(new)
+                elif new > 0:
+                    res.append(new)
+                else:
+                    while res and res[-1] > 0:
+                        if res[-1] + new == 0:
+                            res.pop()
+                            break
+                        elif res[-1] + new < 0:
+                            res.pop()
+                            if not res or res[-1] < 0:
+                                res.append(new)
+                                break
+                        else:
+                            break
+        return res
+
+
+s = Solution()
+s.asteroidCollision([-2, -1, 1, 2])
+
+"""
+TESTCASE #1
+s.asteroidCollision([5, 10, -5])
+
+TESTCASE #2
+s.asteroidCollision([8, -8])
+"""
